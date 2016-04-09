ab6f0cda13c50145771df6eb51f635a4406ddd2b
Bertholet Damien <bertholet.damien@gmail.com>
2016-04-09 12:02:13 -0400

    First draw for a lot of docs

--- public/js/elaiJSApiMain.js
+++ public/js/elaiJSApiMain.js
@@ -41,12 +41,38 @@ define(["elaiJS/configuration", "elaiJS/navigator", "elaiJS/widget"],
 
 /*
   TODO:
-    Creer les pages necessaires
+    Overview
+    RequireElaiJS
+    Configuration
+      DefaultConfiguration
+    Binder
+    Webservice
+      DefaultWebservice
+    Widget
+      primaryWidget
+    Plugin
+    Renderer
+      Mustacherend
+      Polymer
+    Navigation
+    Internationalisation
+      Language
+      Localisation
+    Ressources
+    Mode
+    Theme
+    Helper
+    DebugManager
+    
+    Storage ?
+    
     Ecrire la page de Download
     Ecrire la page d'acceuil
     Ecrire la documentation
     
+    Getting Started?
+    
     Integrer ElaiJS avec bower
     Add ReadMe into GitHub
-    Read about compression gzip
+    Read about compression gzip to use properly
 */
